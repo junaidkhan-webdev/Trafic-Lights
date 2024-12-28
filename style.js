3d4302eb8fe717d3525524e771e2f0c3c581d64b
Junaid Khan <junadikhan.webdev@gmail.com>
2024-12-29 00:23:38 +0500

    add all

---
+++ style.js
@@ -0,0 +1,11 @@
+function red(){
+    alert("stop Becouse The Trafic Lighy Is Red")
+}
+
+function green(){
+    alert("Now You Can Go Becase The Trafic Light Is Green")
+}
+
+function yellow(){
+    alert("Get Redy Becouse The The Trafic Light Is Going To Yellow")
+}
